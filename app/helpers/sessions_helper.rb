9a4498bbb34d5845a3cd5e9cc75a4ebe44f34856
sidemt <25644062+sidemt@users.noreply.github.com>
2020-01-03 07:43:17 +0900

    Add tests for current_user helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,7 +13,7 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  # 現在ログイン中のユーザーを返す（いる場合）
+  # 記憶トークンcookieに対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
