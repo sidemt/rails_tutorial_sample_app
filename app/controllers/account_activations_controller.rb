662a1552906539882f78525c653617b716ea0c2a
sidemt <25644062+sidemt@users.noreply.github.com>
2020-01-03 13:07:53 +0900

    Implement account activation

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,21 @@
 class AccountActivationsController < ApplicationController
+
+  def edit
+    user = User.find_by(email: params[:email])
+    # ユーザーが存在 かつ 有効化前 かつ
+    # params[:id]で渡されたトークンから生成したハッシュ値がダイジェストと一致したら
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      # 有効化状態にする
+      user.update_attribute(:activated, true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      # ログインさせる
+      log_in user
+      flash[:success] = "Account activated!"
+      redirect_to user
+    else
+      # 有効化失敗
+      flash[:danger] = "Invalid activation link"
+      redirect_to root_url
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,22 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # ユーザーログイン
-      log_in user
-      # Remember me チェックボックスの値に応じて処理
-      # チェックしていたら、記憶トークンを生成してダイジェストをデータベースに保存
-      # +トークンをCookieに保存
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      # ユーザー情報のページか、元のページにリダイレクトする
-      redirect_back_or user
+      if user.activated?
+        # ユーザーログイン
+        log_in user
+        # Remember me チェックボックスの値に応じて処理
+        # チェックしていたら、記憶トークンを生成してダイジェストをデータベースに保存
+        # +トークンをCookieに保存
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        # ユーザー情報のページか、元のページにリダイレクトする
+        redirect_back_or user
+      else
+        # アカウントが有効化されていない
+        message = "Account not activated."
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
